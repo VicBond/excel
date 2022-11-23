a6ec35a37d3e0c08371c834ae9ace822f72a0f16
Victor Bondarenko <nirasimha@gmail.com>
2022-11-23 12:51:53 -0500

    add #app selector/ obj with components arr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Excel } from '@/components/excel/Excel';
 import './scss/index.scss';
 
-const excel = new Excel();
+const excel = new Excel('#app', {
+  components: []
+});
 
 console.log('Excel', excel);
